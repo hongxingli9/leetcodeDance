aeb990010640ced26614ee97603316abf969faba
hongxingli9 <homhin@163.com>
2021-08-07 23:17:13 +0800

    优化

--- lc-offer/lco-25-mergeTwoLists.js
+++ lc-offer/lco-25-mergeTwoLists.js
@@ -26,14 +26,13 @@ var mergeTwoLists = function(l1, l2) {
     while (l1 && l2) {
         if (l1.val <= l2.val) {
             cur.next = l1
-            cur = cur.next
             l1 = l1.next
         } else {
             cur.next = l2
-            cur = cur.next
             l2 = l2.next
         }
+        cur = cur.next
     }
-    cur.next = l1 ? l1 : l2 ? l2 : null
+    cur.next = l1 !== null ? l1 : l2
     return head.next
 };
